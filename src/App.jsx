0d0d30bdc8076079e645fdba43e8080a45647931
ALNoman78 <abdullaalno078@gmail.com>
2025-01-24 20:47:28 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 >Vite + React</h1>
+        <p className='text-9xl font-bold'>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button className='btn btn-success'>Click Me</button>
+    </>
+  )
+}
+
+export default App
