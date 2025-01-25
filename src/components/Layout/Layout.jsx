4acdafe5c961da0c0dd1d5661c3bb822705582af
ALNoman78 <abdullaalno078@gmail.com>
2025-01-25 22:28:01 +0600

    react router added

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import Navbar from '../Navbar/Navbar'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Footer/Footer'
+
+const Layout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    )
+}
+
+Layout.propTypes = {}
+
+export default Layout
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './components/Layout/Layout';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
